e673d10dfc4a3dcd1d34920cd391f50c1aaab92a
TheOrijinnn <lenguyensonghinh@gmail.com>
2025-10-17 14:53:01 +0700

    Update db.js

---
+++ src/utils/db.js
@@ -0,0 +1,15 @@
+import knex from 'knex';
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: 'aws-1-us-east-2.pooler.supabase.com',
+    port: 5432,
+    user: 'postgres.hxyhqfgkvgexkqmxrrdc',
+    password: '4h5_C5H7LkprX*T',
+    database: 'postgres',
+    pool: { min: 0, max: 15 },
+  }
+});
+
+export default db;

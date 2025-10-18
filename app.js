3c965e230fba916d9eefffbcfbccb105e5b35663
Chu Ngọc Việt Đức <23110016@student.hcmute.edu.vn>
2025-10-18 21:32:41 +0700

    chuyển html sang hbs

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import express from 'express';
 import { engine } from 'express-handlebars';
 import hbs_sections from 'express-handlebars-sections';
 import session from 'express-session';
+import courseRoute from './src/routes/course.route.js';
+import accountRoute from './src/routes/account.route.js';
+import categoryRoute from './src/routes/category.router.js';
+
 
 const app = express();
 const port = 3000;
@@ -10,8 +14,14 @@ app.engine('hbs', engine({
     extname: '.hbs', 
 }));
 app.set('view engine', 'hbs'); 
-app.set('views', './views');
+app.set('views', './src/views');
 
+app.use(express.static('src/public'));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', courseRoute);
+app.use('/category', categoryRoute);
+app.use('/Account', accountRoute);
 
 app.listen(port, function () {
     console.log(`Server is running on port ${port}`);

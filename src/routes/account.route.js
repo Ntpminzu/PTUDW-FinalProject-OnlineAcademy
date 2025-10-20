8212bd62fd9f049697cc8ca7418d6f91f30c05da
Chu Ngọc Việt Đức <23110016@student.hcmute.edu.vn>
2025-10-20 15:32:36 +0700

    them hbs của folder account

--- src/routes/account.route.js
+++ src/routes/account.route.js
@@ -9,5 +9,13 @@ router.get('/signin', (req, res) => {
   res.render('account/signin');
 });
 
+router.get('/profile', (req, res) => {
+  res.render('account/profile');
+});
+
+router.get('/change-password', (req, res) => {
+  res.render('account/change-password');
+});
+
 export default router;
 
